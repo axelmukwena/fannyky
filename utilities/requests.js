9f5f1b453d92433b506a344b58911f3ec0783ff2
Axel Mukwena <axel.muk@gmail.com>
2022-02-11 11:28:23 +0800

    Update requests.js

--- utilities/requests.js
+++ utilities/requests.js
@@ -10,7 +10,7 @@ function handleMissingRecords(message) {
 // Get data from api using timeout
 export async function fetchWithTimeout(url) {
   const controller = new AbortController();
-  const timeout = 8000;
+  const timeout = 7000;
 
   const id = setTimeout(() => {
     controller.abort();
@@ -39,6 +39,7 @@ export async function fetchWithTimeout(url) {
     });
 
   clearTimeout(id);
+  console.log("Fetch Response:", response);
   return response;
 }
 
